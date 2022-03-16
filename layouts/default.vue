d33b052faed75a959e260c686e9287ee59882d24
Lirac <Liraclomex@yahoo.com>
2022-03-16 23:45:36 +0100

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" fixed app>
+      <v-list-item class="mb-10">
+        <v-list-item-avatar>
+          <img src="~/assets/img/avatar.svg" />
+        </v-list-item-avatar>
+      </v-list-item>
+      <v-list nav dense>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+          active-class="list_item_active"
+          class="list_item"
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed flat app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-spacer />
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light> mdi-repeat </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Dashboard',
+          to: '/',
+        },
+        {
+          icon: 'mdi-hand-coin',
+          title: 'Get Loans',
+          to: '/inspire',
+        },
+        {
+          icon: 'mdi-send',
+          title: 'Transfer Money',
+          to: '',
+        },
+        {
+          icon: 'mdi-piggy-bank',
+          title: 'Save Money',
+          to: '',
+        },
+        {
+          icon: 'mdi-wallet',
+          title: 'Wallet',
+          to: '',
+        },
+        {
+          icon: 'mdi-cart',
+          title: 'Pay Later',
+          to: '',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
+<style scoped>
+.list_item {
+  margin: 1rem 1rem;
+}
+.list_item_active {
+  background: #6eb04d;
+}
+</style>
